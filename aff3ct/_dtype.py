3ad42fd95b7b5aae1e67923ee60c28b5edb26e29
TAJAN Romain <Romain.Tajan@bordeaux-inp.fr>
2023-11-04 00:51:32 +0100

    Flake8 improvement of _dtype.py

--- aff3ct/_dtype.py
+++ aff3ct/_dtype.py
@@ -1,14 +1,56 @@
-from aff3ct._ext import dtype, uint8, uint16, uint32, uint64, int8, int16, int32, int64, float32, float64
-from aff3ct._ext.core import Socket
+# encoding: utf-8
+"""Add some python magic for handling dtypes."""
+
+from __future__ import annotations
+
+from typing import Union
+
+import numpy as np
+
+from . import _ext
 from ._array import array
 
-def __dtype_call__(slf, data):
-    if type(data) is Socket:
-        return data.astype(slf)
-    else:
-        return array(data, dtype = slf)
+dtype   = _ext.dtype
+uint8   = _ext.uint8
+uint16  = _ext.uint16
+uint32  = _ext.uint32
+uint64  = _ext.uint64
+int8    = _ext.int8
+int16   = _ext.int16
+int32   = _ext.int32
+int64   = _ext.int64
+float32 = _ext.float32
+float64 = _ext.float64
+Socket  = _ext.core.Socket
 
-dtype.__call__ = lambda slf, data: __dtype_call__(slf, data)
 
-__all__ = ['dtype', 'uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16',
-           'int32', 'int64', 'float32', 'float64']
+def _dtype_call(self: dtype, data: Union['Socket', 'np.array']) -> Socket:
+    """Create socket representing `data` with the dtype of `self`.
+
+    Args:
+        self (dtype): A dtype object
+        data (Socket): A socket or a numpy array to be casted.
+
+    Returns:
+        out (Socket): a socket representing data with the `dtype` of `self`
+    """
+    if isinstance(data, Socket):
+        return data.astype(self)
+
+    return array(data, dtype=self)
+
+
+dtype.__call__ = _dtype_call
+
+__all__ = [
+    'dtype',
+    'uint8',
+    'uint16',
+    'uint32',
+    'uint64',
+    'int8',
+    'int16',
+    'int32',
+    'int64',
+    'float32',
+    'float64']

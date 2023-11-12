ce9ecbe43bf4edde8a048f0de8b953c0171cabf7
TAJAN Romain <Romain.Tajan@bordeaux-inp.fr>
2023-11-12 10:48:27 +0100

    Update tests.

--- aff3ct/test/test_array.py
+++ aff3ct/test/test_array.py
@@ -2,19 +2,19 @@ from aff3ct import array, arange
 import numpy as np
 
 def test_array_type(dtype):
-    x = array([1,2,3,4], 14, dtype = dtype)
-    assert(x.dtype.name == dtype.name)
+    x = array([1, 2, 3, 4], 14, dtype = dtype)
+    assert (x.dtype.name == dtype.name)
 
 def test_array_value(dtype):
     x = array([25], 14, dtype=dtype)
-    assert(x.numpy()[0] == dtype.numpy(25))
+    assert (x.numpy()[0] == dtype.numpy(25))
 
 def test_array_size(dtype):
     x = array([25], 14, dtype = dtype)
-    assert(x.n_elmts == 14)
+    assert (x.n_elmts == 14)
 
 def test_arange(dtype):
     x  = np  .arange(0, 255, 2.3, dtype = dtype.numpy)
     px = arange(0, 255, 2.3, dtype = dtype)
 
-    assert(np.allclose(px[:], x[:]))
+    assert (np.allclose(px[:], x[:]))

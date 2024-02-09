6fd821108a1e8d52b687ac09c0243547bbe90942
TAJAN Romain <Romain.Tajan@bordeaux-inp.fr>
2024-02-09 11:45:55 +0100

    Update AFF3CT + improve wrapping of interfaces

--- src/wrapper/Tools/Interface/Interface_reset.cpp
+++ src/wrapper/Tools/Interface/Interface_reset.cpp
@@ -6,6 +6,9 @@ using namespace pyaf::wrapper;
 
 void pyaf::wrapper::wrap_interface_reset(py::module_& scope)
 {
-	auto itf_class = py::class_<aff3ct::tools::Interface_reset, pyaf::wrapper::py_Interface_reset, std::shared_ptr<aff3ct::tools::Interface_reset>>(scope, "Interface_reset");
+	auto itf_class = py::class_<aff3ct::tools::Interface_reset,
+	                            pyaf::wrapper::py_Interface_reset,
+								std::unique_ptr<aff3ct::tools::Interface_reset, py::nodelete>
+								>(scope, "Interface_reset");
 	itf_class.def("reset", &aff3ct::tools::Interface_reset::reset);
 }

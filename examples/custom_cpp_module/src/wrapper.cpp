ec38e05c82a866ac16b26acc8be16f56236f955b
Romain Tajan <romain.tajan@ims-bordeaux.fr>
2025-10-08 12:27:01 +0200

    Use external pybind11==3.0.1 version instead of git submodule.

--- examples/custom_cpp_module/src/wrapper.cpp
+++ examples/custom_cpp_module/src/wrapper.cpp
@@ -8,6 +8,8 @@ using namespace py::literals;
 // Create a	python module using PYBIND11, here our module will be named pyaf
 PYBIND11_MODULE(my_module, m)
 {
-    auto pyspu_stateful = (py::object)py::module_::import("streampu").attr("Stateful");
+    auto pyspu_stateful = (py::object)py::module_::import("streampu");
+    // spu::tools::Buffer_allocator::set_task_autoalloc(true);
+    m.attr("spu") = pyspu_stateful;
     py::class_<Adder, spu::module::Stateful>(m, "Adder").def(py::init<const int>(), "n_elmts"_a);
 }
